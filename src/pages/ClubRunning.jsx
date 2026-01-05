625cabffd90dd70212eaf42452565539462f8e7f
kai <iannai20021021@gmail.com>
2026-01-06 00:25:22 +0800

    fix: remove duplicate imports triggering build error

--- src/pages/ClubRunning.jsx
+++ src/pages/ClubRunning.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-
-import Footer from '../components/Footer';
 import { getAssetPath } from '../utils/assets';
 
 const ClubRunning = () => {
